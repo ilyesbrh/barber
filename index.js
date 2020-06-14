7df9674ebef29c820b306b8c88d8eacf7f29f019
desktop <brhilyes@gmail.com>
2020-06-14 21:11:44 +0100

    first push

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-$('.scroll').perfectScrollbar();
+function init() {
+
+    let list = document.body.getElementsByClassName('perfect-scroll');
+    for (const div of list) {
+        const ps = new PerfectScrollbar(div);
+        console.log(ps);
+
+    }
+
+}
+init();
